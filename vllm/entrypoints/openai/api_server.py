486e0f67d5489501ba7fb50697abf9340d80903f
Wei Da <daweiuestc@gmail.com>
2025-11-28 19:59:58 +0000

    simplified the output of each requests

--- vllm/entrypoints/openai/api_server.py
+++ vllm/entrypoints/openai/api_server.py
@@ -395,15 +395,11 @@ async def schedule_trace(raw_request: Request):
         else:
             scheduler_trace_flattened[key] = scheduler_trace[key]
             for request_info in scheduler_trace[key]:
-                request_id = request_info['request_id']
-                arrival_time = request_info["arrival_time"]
                 num_prompt_tokens = request_info["num_prompt_tokens"]
                 num_computed_tokens = request_info["num_computed_tokens"]
                 total_num_tokens = request_info["total_num_tokens"]
-                n_blocks = request_info["n_blocks"]
-                scheduler_trace_flattened[key].extend([request_id, arrival_time,
-                                                         num_prompt_tokens, num_computed_tokens,
-                                                         total_num_tokens, n_blocks])
+                scheduler_trace_flattened[key].extend([num_prompt_tokens, num_computed_tokens,
+                                                       total_num_tokens])
     scheduler_trace_flattened["free_gpu_blocks"] = free_gpu_blocks
     scheduler_trace_flattened["num_preempted"] = num_preempted
     encoded_scheduler_trace = orjson.dumps(scheduler_trace_flattened)
